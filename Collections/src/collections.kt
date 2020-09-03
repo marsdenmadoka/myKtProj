abf20f2c02568d45fb5faf60449c5cd40df68b99
marsdenmadoka <marsmadoka98@gmail.com>
2020-09-03 17:34:56 +0300

    added new files

--- Collections/src/collections.kt
+++ Collections/src/collections.kt
@@ -52,39 +52,49 @@
 //key/value pairs using toList like this:
 data class Recipe(var name:String) //add the recipe data class
 fun main(args:Array<String>){
-    var mShopingList= mutableListOf("Tea","Eggs","Milk")
-    println("mShopping original: $mShopingList")
-    val extraShopping = listOf("Cookies","sugar","Eggs")
-    mShopingList.addAll(extraShopping)
-    println("mShoppingList items added: $mShopingList")
-    if(mShopingList.contains("Tea")){
-        mShopingList.set(mShopingList.indexOf("Tea"),"Coffee")
-    }
-    mShopingList.sort()
-    println("mShopping list sorted $mShopingList")
-    mShopingList.reverse()
-    println("mShoppingList reversed $mShopingList")
-
-    val mShoppingSet=mShopingList.toMutableSet() //copying our list into a set
-    println("Mshopping set : $mShoppingSet")
-    val moreShopping= setOf("Chives","Spinach","Milk")
-    mShoppingSet.addAll(moreShopping)
-    println("mShoppingSet items added: $mShoppingSet")
-    mShopingList=mShoppingSet.toMutableList()
-    println("mShoppingList new version: $mShopingList")
     //set
     val r1=Recipe("Chicken Soup")
     val r2=Recipe("Quinoa Salad")
     val r3=Recipe("Thai Curry")
     val r4=Recipe("Jambalaya")
-    var r5=Recipe("Sausage Rolls")
+    val r5=Recipe("Sausage Rolls")
     val mRecipeMap= mutableMapOf("Recipe1" to r1,"Recipe2" to r2,"Recipe3" to r3)
      println("mRecipeMap original: $mRecipeMap")
     val recipesToAdd= mapOf("Recipe4" to r4,"Recipe5" to r5)
-    mRecipeMap.putAll(recipesToAdd)
+    mRecipeMap.putAll(recipesToAdd) //put our first hashMap to the second after we have updated
     println("mRecipeMap updated: $mRecipeMap")
      if (mRecipeMap.containsKey("Recipe1")){
          println("Recipe1 is: ${mRecipeMap.getValue("Recipe1")}")
      }
 }
 
+
+//fun main(args: Array<String>) {
+//    val term1 = "Array"
+//    val term2 = "List"
+//    val term3 = "Map"
+//    val term4 = "MutableList"
+//    val term5 = "MutableMap"
+//    val term6 = "MutableSet"
+//    val term7 = "Set"
+//
+//    val def1 = "Holds values in no particular order."
+//    val def2 = "Holds key/value pairs."
+//    val def3 = "Holds values in a sequence."
+//    val def4 = "Can be updated."
+//    val def5 = "Can't be updated."
+//    val def6 = "Can be resized."
+//    val def7 = "Can't be resized."
+//
+//      val glossary = mapOf(
+//        term4 to "$def3 $def4 $def6",
+//        term7 to "$def1 $def5 $def7",
+//        term1 to "$def3 $def4 $def7",
+//        term5 to "$def2 $def4 $def6",
+//        term2 to "$def3 $def5 $def7",
+//        term6 to "$def1 $def4 $def6",
+//        term3 to "$def2 $def5 $def7")
+//
+//        for ((key, value) in glossary)
+//        println("$key: $value")
+//}
