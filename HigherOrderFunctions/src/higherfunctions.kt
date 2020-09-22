0961760a290336263d3a07cfe98297f74df5390b
marsdenmadoka <marsmadoka98@gmail.com>
2020-09-22 11:16:43 +0300

    added new files

--- HigherOrderFunctions/src/higherfunctions.kt
+++ HigherOrderFunctions/src/higherfunctions.kt
@@ -1,3 +1,35 @@
+// data class Grocery(val name:String,
+//                   val category:String,
+//                   val unit:String,
+//                   val unitPrice:Double,
+//                   val quantity:Int
+//)
+//fun main(args:Array<String>){
+//    val groceries= listOf(Grocery("Tomatoes","Vegetable","lb",3.0,3),
+//                      Grocery("Mushrooms","Vegetable","lb",4.0,1),
+//                      Grocery("Bagels","Bakery","pack",1.5,2),
+//                      Grocery("Olive oil","Pantry","Bottle",6.0,1),
+//                      Grocery("Ice cream","Frozen","Pack",3.0,2)
+//    )
+//
+//    val highestUnitPrice=groceries.maxBy { it.unitPrice} //{ it.unitPrice*5}
+//    println("highestUnitPrice:$highestUnitPrice")
+//    val lowestQuantity=groceries.minBy { it.quantity }
+//    println("lowesrQuantity:$lowestQuantity")
+//
+//    val sumQuantity=groceries.sumBy { it.quantity }
+//    println("sumQuantity:$sumQuantity")
+//    val totalPrice=groceries.sumByDouble { it.quantity * it.unitPrice }
+//    println("totalPrice:$totalPrice")
+//
+//
+//
+//}
+//
+
+
+
+
 data class Grocery(val name:String,
                    val category:String,
                    val unit:String,
@@ -6,21 +38,38 @@ data class Grocery(val name:String,
 )
 fun main(args:Array<String>){
     val groceries= listOf(Grocery("Tomatoes","Vegetable","lb",3.0,3),
-                      Grocery("Mushrooms","Vegetable","lb",4.0,1),
-                      Grocery("Bagels","Bakery","pack",1.5,2),
-                      Grocery("Olive oil","Pantry","Bottle",6.0,1),
-                      Grocery("Ice cream","Frozen","Pack",3.0,2)
+            Grocery("Mushrooms","Vegetable","lb",4.0,1),
+            Grocery("Bagels","Bakery","pack",1.5,2),
+            Grocery("Olive oil","Pantry","Bottle",6.0,1),
+            Grocery("Ice cream","Frozen","Pack",3.0,2)
     )
+    val vegetables=groceries.filter { it.category == "Vegetable" }
+   println("vegetables: $vegetables")
 
-    val highestUnitPrice=groceries.maxBy { it.unitPrice} //{ it.unitPrice*5}
-    println("highestUnitPrice:$highestUnitPrice")
-    val lowestQuantity=groceries.minBy { it.quantity }
-    println("lowesrQuantity:$lowestQuantity")
+    val notFrozen=groceries.filterNot { it.category== "Frozen" }
+    println("notFrozen: $notFrozen")
 
-    val sumQuantity=groceries.sumBy { it.quantity }
-    println("sumQuantity:$sumQuantity")
-    val totalPrice=groceries.sumByDouble { it.quantity * it.unitPrice }
-    println("totalPrice:$totalPrice")
+    val groceryNames=groceries.map { it.name }
+    println("groceryNames: $groceryNames")
+
+    val halfUnitPrice=groceries.map{it.unitPrice * 0.5}
+    println("halfUnitPrice: $halfUnitPrice")
+
+     val newPrices=groceries.filter { it.unitPrice>3.0 }
+             .map { it.unitPrice * 2 }
+      println("newPrices: $newPrices")
+
+    println("Grocery names: ")
+    groceries.forEach { println(it.name) }
+
+    println("Groceries with unitPrice > 3.0: ")
+    groceries.filter { it.unitPrice>3.0 }
+            .forEach { println(it.name) }
+
+
+    var itemNames= ""
+    groceries.forEach ({ itemNames += " ${it.name}" })
+    println("itemNames: $itemNames")
 
 
 
